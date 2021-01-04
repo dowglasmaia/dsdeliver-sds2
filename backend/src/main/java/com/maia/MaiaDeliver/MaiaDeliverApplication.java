0c7e662d3c8e616a6ffc679fe58da7f26f313575
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-04 10:39:58 -0300

    Project created

---
+++ backend/src/main/java/com/maia/MaiaDeliver/MaiaDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.maia.MaiaDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiaDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiaDeliverApplication.class, args);
+	}
+
+}
