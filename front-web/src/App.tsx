82ae036a57ab2c6c38637f2eb6dcb88af6e3268c
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-06 18:26:34 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
     <div >
-     <h1>Hello word!</h1>
+      
+     <Navbar/>
+
     </div>
   );
 }
