99a5fb9fdc86f1365648cfae95e451d27d5f8aa7
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-07 09:48:06 -0300

    Add Footer

--- front-web/src/components/Home/index.tsx
+++ front-web/src/components/Home/index.tsx
@@ -1,35 +1,43 @@
 import "./styles.css"
+import Footer from "../Footer";
 
 import { ReactComponent as MainImage } from './main.svg';
 
 function Home() {
     return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br />
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br />
                         que entregamos  <br />
                         pra você!!!
                     </h1>
 
-                    <h4 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos <br />
-                     levaremoss na sua porta
-                    </h4>
+                        <h4 className="home-subtitle">
+                            Escolha o seu pedido e em poucos minutos <br />
+                            levaremoss na sua porta
+                         </h4>
 
-                    <a href="#" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
+                        <a href="#" className="home-btn-order">
+                            FAZER PEDIDO
+                        </a>
+
+                    </div>
+
+                    <div className="home-img" >
+                        <MainImage />
+                    </div>
 
                 </div>
-
-                <div className="home-img" >
-                    <MainImage/>
-                </div>
-
             </div>
-        </div>
+
+            <Footer/>
+            
+
+
+        </>
     )
 
 }
