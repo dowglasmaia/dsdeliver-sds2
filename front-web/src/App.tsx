1dee112df00406063a4ff2cd7ef7ca88ebb44958
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-06 23:06:19 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css';
+
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 
 
 function App() {
   return (
-    <div >
-      
-     <Navbar/>
+    <>
 
-    </div>
+      <Navbar />
+      <Home />
+
+    </>
   );
 }
 
---
+++ front-web/src/components/Home/index.tsx
@@ -0,0 +1,36 @@
+import "./styles.css"
+
+import { ReactComponent as MainImage } from './main.svg';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br />
+                        que entregamos  <br />
+                        pra você!!!
+                    </h1>
+
+                    <h4 className="home-subtitle">
+                        Escolha o seu pedido e em poucos minutos <br />
+                     levaremoss na sua porta
+                    </h4>
+
+                    <a href="#" className="home-btn-order">
+                        FAZER PEDIDO
+                    </a>
+
+                </div>
+
+                <div className="home-img" >
+                    <MainImage/>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+export default Home;
