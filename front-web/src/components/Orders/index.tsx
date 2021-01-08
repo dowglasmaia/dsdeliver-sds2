701a7ed8cd125103868dc172b4695b1b57214a9c
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-07 23:05:41 -0300

    Criando Card de Produtos

--- front-web/src/components/Orders/index.tsx
+++ front-web/src/components/Orders/index.tsx
@@ -1,14 +1,17 @@
 import "./styles.css"
 
 import StepsHeader from "./StepsHeader"
+import ProductsList from "./ProductsList"
 
 //import { ReactComponent as MainImage } from './main.svg';
 
 function Order() {
     return (
         <div className="orders-container">
-           
-           <StepsHeader/>
+
+            <StepsHeader />
+
+            <ProductsList />
 
         </div>
     )
